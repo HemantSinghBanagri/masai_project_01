df63106b5af465443825ad864435c47dbdf13c50
darkkira <hemant2307sb@gmail.com>
2022-10-23 12:18:49 +0530

    this is a login file

---
+++ login.js
@@ -0,0 +1,9 @@
+let reg_mail="hemant2307@gmail.com"
+let reg_pass="hemant"
+
+let login_mail="hemant23"
+let login_pass="hemant23"
+
+if(reg_mail==login_mail && reg_pass==login_pass){
+console.log("Login Sucessfully")
+}
